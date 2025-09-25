72982f63e48c37a9769ad15e3a0e0e16674cefd4
iotaillusion <iotaillusion@gmail.com>
2025-09-25 16:40:42 -0400

    Delete services directory

--- services/rule-engine/jest.config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.js'],
-  clearMocks: true
-};
--- services/scene-svc/src/index.js
+++
@@ -1,6 +0,0 @@
-const { startServer } = require('./rest');
-
-const port = Number.parseInt(process.env.PORT || '', 10) || undefined;
-const deviceRegistryUrl = process.env.DEVICE_REGISTRY_URL;
-
-startServer({ port, deviceRegistryUrl });
